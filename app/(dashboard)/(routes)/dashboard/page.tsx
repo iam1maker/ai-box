83e43b51780a6c8b082b3bd7880bfd7460a26c77
1maker <383679422@qq.com>
2024-04-26 01:01:14 +0800

    finish folder Setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <p className=" text-6xl text-green-500">
+                Dashboard Page(protected)
+            </p>
+        </div>
+    );
+}

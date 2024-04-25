33db2e0331879d036cba6a30fa728c997a7cc2e9
1maker <383679422@qq.com>
2024-04-25 15:37:09 +0800

    "project init"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <div>
+            <Button variant={"destructive"} size={"lg"}>
+                click me
+            </Button>
             <p className=" text-6xl text-green-500">Hello AI Box!</p>
         </div>
     );
